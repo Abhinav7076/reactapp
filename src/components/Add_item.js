3bb372dd78f696fddd3ddeaf2bb80b97d0eef03a
Abhinav Singh <abhinav.singh@tigeranalytics.local>
2022-12-22 17:57:36 +0530

    part 2 completed

--- src/components/Add_item.js
+++ src/components/Add_item.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import { useNavigate} from 'react-router-dom'
 import './Add_item.css'
@@ -9,7 +9,12 @@ import axios from 'axios'
 
 const Add_item = () => {
     const navigate = useNavigate()
-
+    const [title, setTitle] = useState({val: '', valid: false})
+    const [link, setLink] = useState({val: '', valid: false})
+    const [icon_url, setIcon_url] = useState({val: '', valid: false})
+    const [category, setCategory] = useState({val: '', valid: false})
+    const [description, setDescription] = useState({val: '', valid: true})
+    
     const errorMessage = (message) =>{
         toast.error(message,{
             position: 'bottom-center',
@@ -18,24 +23,8 @@ const Add_item = () => {
     }
 
     const submitFunc= (event)=>{
-        const title = event.target[0].value
-        const link = event.target[1].value
-        const icon_url = event.target[2].value
-        const category = event.target[4].value
-        const description = event.target[5].value
-
         event.preventDefault()
-
-        if(title.length === 0) errorMessage('Please enter the title!')
-        else if(link.length === 0) errorMessage('Please enter the link!')
-        else if(!validator.isURL(link)) errorMessage('Link not valid!')
-        else if(icon_url.length === 0) errorMessage('Please enter the icon URL!')
-        else if(!validator.isURL(icon_url)) errorMessage('Icon URL not valid!')
-        else if(category.length === 0) errorMessage('Please enter the category!')
-        else if(description.length === 0) errorMessage('Please enter the description!')
-        else if(description.length > 100) errorMessage('Description length should not exceed 100 characters!')
-        else{
-            const url = 'https://media-content.ccbp.in/website/react-assignment/add_resource.json'
+        const url = 'https://media-content.ccbp.in/website/react-assignment/add_resource.json'
             axios.get(url).then(response => {
             
                 let statusCode = response.status
@@ -48,6 +37,33 @@ const Add_item = () => {
             }).catch((error) => {
                 errorMessage(error.message)
             })
+        
+    }
+
+    const handleInput = (event)=>{
+        // console.log(event.target.value)
+        // console.log(event.target.name)
+        if(event.target.name === 'title'){
+            setTitle({val: event.target.value, valid: true})
+        }
+        if(event.target.name === 'link'){
+            if(validator.isURL(event.target.value))
+            setLink({val: event.target.value, valid: true})
+            else
+            setLink({val: event.target.value, valid: false})
+
+        }
+        if(event.target.name === 'icon_url'){
+            if(validator.isURL(event.target.value))
+            setIcon_url({val: event.target.value, valid: true})
+            else
+            setIcon_url({val: event.target.value, valid: false})
+        }
+        if(event.target.name === 'category'){
+            setCategory({val: event.target.value, valid: true})
+        }
+        if(event.target.name === 'description'){
+            setDescription({val: event.target.value, valid: true})
         }
     }
 
@@ -62,17 +78,21 @@ const Add_item = () => {
             <form onSubmit={submitFunc}>
             <div className="form-group fmx">
                 <label className='lbx'>ITEM TITLE</label>
-                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter title" />
+                <input name='title' type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter title" onChange={handleInput} />
             </div>
 
             <div className="form-group fmx">
                 <label className='lbx'>LINK</label>
-                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter link" />
+                <input name='link' type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter link" onChange={handleInput} />
+                {(link.val.length===0 || link.valid) ? null : 
+                <p style={{color: 'red', marginLeft: '30px', fontSize: '13px', marginTop: '5px'}}>Link not valid</p>}
             </div>
 
             <div className="form-group fmx">
                 <label className='lbx'>ICON URL</label>
-                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter icon url" />
+                <input name='icon_url' type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter icon url" onChange={handleInput} />
+                {(icon_url.val.length===0 || icon_url.valid) ? null : 
+                <p style={{color: 'red', marginLeft: '30px', fontSize: '13px', marginTop: '5px'}}>Icon URL not valid</p>}
             </div>
 
             <label style={{color: 'grey'}}>TAG NAME</label>
@@ -83,14 +103,17 @@ const Add_item = () => {
 
             <div className="form-group fmx">
                 <label className='lbx'>CATEGORY</label>
-                <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter category" />
+                <input name='category' type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter category" onChange={handleInput} />
             </div>
 
             <div className="form-group fmx">
                 <label className='lbx'>DESCRIPTION</label>
-                <textarea className="form-control bradius" id="exampleFormControlTextarea1" rows="3" placeholder='Enter description...'></textarea>
+                <textarea name='description' className="form-control bradius" id="exampleFormControlTextarea1" rows="3" placeholder='Enter description...' onChange={handleInput}></textarea>
+                {(description.val.length<=100) ? null :
+                <p style={{color: 'red', marginLeft: '30px', fontSize: '13px', marginTop: '5px'}}>Description length should not exceed 100 characters! {'( '} {100-description.val.length} {' )'}</p>}
             </div>
-                <input type="submit" value="CREATE" style={{marginLeft: '190px'}} />
+                <input type="submit" value="CREATE" disabled={title.val.length===0 || category.val.length===0 || description.val.length>100 || !link.valid || !icon_url.valid} 
+                style={(title.val.length===0 || category.val.length===0 || description.val.length>100 || !link.valid || !icon_url.valid) ? {cursor: 'not-allowed'}: {cursor: 'pointer'}} />
             </form>
             </div>
             <div className="split right">
